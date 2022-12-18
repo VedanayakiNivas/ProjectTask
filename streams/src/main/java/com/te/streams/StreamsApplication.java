1cb285ca689fafa27e63900c93690e81401f6063
Vedanayaki <vedhaa20@gmail.com>
2022-12-19 02:00:28 +0530

    forteenth commit

--- streams/src/main/java/com/te/streams/StreamsApplication.java
+++ streams/src/main/java/com/te/streams/StreamsApplication.java
@@ -2,9 +2,10 @@ package com.te.streams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
-public class StreamsApplication {
+public class StreamsApplication extends SpringServletContainerInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StreamsApplication.class, args);
